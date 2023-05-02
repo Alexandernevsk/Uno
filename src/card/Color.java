49fbe4f458241ae69285ebe3f69cf1296fd5b690
aegroot <alex-groot@live.nl>
2023-05-02 14:12:14 +0200

    player base

--- src/card/Color.java
+++ src/card/Color.java
@@ -2,4 +2,6 @@ package card;
 
 public enum Color {
     yellow,green,blue,red;
+    // TODO: 02/05/2023  associate colors with values
 }
+
--- src/player/Player.java
+++ src/player/Player.java
@@ -1,14 +1,19 @@
 package player;
 
 import card.Card;
+import card.Rank;
 
 import java.util.ArrayList;
 
 public class Player {
     final ArrayList<Card>cards;
 
+
+
     public Player(ArrayList<Card> cards) {
         this.cards = cards;
+
+
     }
     public void playCard(Card card){
         cards.remove(card);
