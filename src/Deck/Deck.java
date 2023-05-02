9eb0c45f6f589606cd8e9fb5eb82dcbbd385f147
Alexandernevsk <Alexandertorreman@gmail.com>
2023-05-02 13:15:58 +0200

    Empty

---
+++ src/Deck/Deck.java
@@ -0,0 +1,17 @@
+package Deck;
+
+import card.Card;
+
+import java.util.ArrayList;
+
+public class Deck {
+    ArrayList<Card> drawPile;
+    ArrayList<Card> discardPile;
+
+    public void createDeck(){
+        for(int rank = 0; rank < 9; rank++){
+
+        }
+    }
+
+}
